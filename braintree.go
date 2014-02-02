3233fb112b87eed2e802fc048fecf4b1b0ab1aa1
Kayle Gishen <KayleGishen@gmail.com>
2014-02-01 23:48:34 -0500

    Version Bump. Merged Upstream

--- braintree.go
+++ braintree.go
@@ -75,7 +75,7 @@ func (g *Braintree) execute(method, path string, xmlObj interface{}) (*Response,
 	req.Header.Set("Content-Type", "application/xml")
 	req.Header.Set("Accept", "application/xml")
 	req.Header.Set("Accept-Encoding", "gzip")
-	req.Header.Set("User-Agent", "Braintree Go 0.3.1")
+	req.Header.Set("User-Agent", "Braintree Go 0.3.2")
 	req.Header.Set("X-ApiVersion", "3")
 	req.SetBasicAuth(g.PublicKey, g.PrivateKey)
 
